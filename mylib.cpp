6c7c8474189110964c1967ea834370f0f4c6529e
Bob Hirosky <hirosky@virginia.edu>
2023-10-14 11:08:18 -0400

    WIP

--- mylib.cpp
+++ mylib.cpp
@@ -11,6 +11,23 @@
 extern "C" {
 #endif
 
+void mandel(double *img, double re1, double re2, double im1, double im2,
+	    int nr, int ni){
+  const int NTRIALS=255;
+  double dx=(re2-re1)/nr;
+  double dy=(im2-im1)/ni;
+  // loop over grid starting in lower left corner
+  for (int j=0; j<ni; ++j){
+    double im=im1+j*dy;
+    for (int i=0; i<nr; ++i){
+      double re=re1+i*dx;
+      img[j*ni+i]=mandel_test(re,im,NTRIALS);
+    }
+  }
+}
+
+
+
 double findPi(long nthrows=100*1000*1000){
   srand48((long)time(NULL));
   long count=0;
@@ -21,12 +38,48 @@ double findPi(long nthrows=100*1000*1000){
   }
   return 4.0*count/nthrows;
 }
+ 
+
+// return the magnitude of a complex number
+double magnitude(double re, double im){
+  return sqrt(re*re+im*im);
+}
+
+
+int mandel_test(double c_re, double c_im, int NTRIALS){  
+  // If a point is in the set, its magnitude will remain bounded by
+  // 2.0 over iterations of z -> z^2 + C.  Stop the loop after a 
+  // maximum of NTRIALS and consider this point to be in the set
+
+  double z_re = c_re;
+  double z_im = c_im;
+  int counts = 1;
+  while (magnitude(z_re,z_im)<=2.0  && counts<NTRIALS) {
+    counts++;
+    double re=z_re;  // careful! keep old values for calculation
+    double im=z_im;
+    // z -> z^2
+    z_re = re*re-im*im;
+    z_im = 2*re*im;
+    // add c to z^2
+    z_re = z_re + c_re;
+    z_im = z_im + c_im;
+  }
+  return counts;
+}
+
+
 
 #ifdef EXTERNC
 }
 #endif
 
+
+
+
+/*
 int main(){
   findPi();
   return 0;
 } 
+*/
--- mylib.h
+++ mylib.h
@@ -1,12 +1,18 @@
 #ifndef MYLIB_H
 #define MYLIB_H
+#include <cmath>
+
 
 #ifdef EXTERNC
 extern "C" {
 #endif
 
 double findPi(long nthrows);
-
+double magnitude(double re, double im);
+int mandel_test(double c_re, double c_im, int NTRIALS=255);
+void mandel(double *img, double re1, double re2, double im1, double im2,
+	    int nr, int ni);
+  
 #ifdef EXTERNC
 }
 #endif
